f5708848cb13f060c0bac2f011d1a07399cdc689
Andre <andrempmattos@gmail.com>
2021-07-08 16:40:04 -0300

    boiler_loop: src: Adding timer abstraction implementation

--- boiler_controller_loop/src/main.c
+++ boiler_controller_loop/src/main.c
@@ -1,24 +1,40 @@
-
-
-#include "controller.h"
-#include "timer.h"
-#include "udp_client.h"
+/**
+ * \brief Main loop implementation.
+ * 
+ * \date 2021/08/07
+ * 
+ * \addtogroup main
+ * \{
+ */
 
 #include <stdio.h>
 
+#include "controller.h"
+#include "udp_client.h"
+#include "timer.h"
 
 int main(int argc, char const *argv[]) {
+	
+	int cycle = 0;
 
+	/* Boiler controller initialization */
 	controller_init();
 
+	/* UDP interface initialization */
 	udp_init();
 
+	/* Timer initialization */
 	timer_init();
 
-
+	/* Main loop (cyclic executive) */
 	while(1) {
-	printf("running!\n");
-		//return 0;
+		
+		/* Do stuff */
+		printf("running! \t %d \n", cycle++);
+		
+		/* Delay until next system loop cycle (system tick) */
+		timer_delay(SYS_TICK);
 	}
-
 }
+
+/** \} End of main group */
--- boiler_controller_loop/src/timer.h
+++ boiler_controller_loop/src/timer.h
@@ -1,7 +1,34 @@
+/**
+ * \brief Timing abstraction definitions.
+ * 
+ * \date 2021/08/07
+ * 
+ * \defgroup timer Timer
+ * \ingroup src
+ * \{
+ */
 
+#ifndef TIMER_H_
+#define TIMER_H_
 
+#include <stdlib.h>
+#include <time.h>
 
+#define NSEC_PER_SEC    (1000000000) 		/* The number of nanoseconds per second. */
+#define SYS_TICK    	(NSEC_PER_SEC/5) 	/* System tick time. */
 
+/**
+ * \brief Timer abstraction initialization.
+ */
 void timer_init(void);
 
-void delay(void);
+/**
+ * \brief Timer delay/sleep function abstraction.
+ *
+ * \param[in] delay_ns is the time in nanoseconds to delay.
+ */
+void timer_delay(long delay_ns);
+
+#endif /* TIMER_H_ */
+
+/** \} End of timer group */
