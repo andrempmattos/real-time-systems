af7eb8fc33ff5fb082e40b541fc21dac45472e93
Lukas Alberto Belck <lukas.belck@gmail.com>
2021-07-09 18:41:14 +0200

    Add boilerplate of how controllers would work

--- boiler_controller_loop/src/main.c
+++ boiler_controller_loop/src/main.c
@@ -18,9 +18,18 @@ int main(int argc, char const *argv[]) {
 	int cycle = 0;
 	char buffer[MAX_BUFFER_SIZE];
 	int received_size = 0;
+	float temp;
+	float ti;
+	float control_na;
+	float control_q;
+	float control_ni;
+
+	float temp_ref = 70;
 
 	/* Boiler controller initialization */
-	controller_init();
+	struct controller na_control = controller_init("Na", "temperature", 10.0, 0.0, 500.0, 0.5);
+	struct controller ni_control = controller_init("Ni", "temperature", 100.0, 0.0, -500.0, 0.5);
+	struct controller q_control = controller_init("Q", "temperature", 1000000.0, 0.0, 10000000.0, 0.5);
 
 	/* UDP interface initialization */
 	udp_init(SERVER_ADDRESS, SERVER_PORT);
@@ -37,6 +46,22 @@ int main(int argc, char const *argv[]) {
 		send_message("st-0");
 		received_size = receive_message(buffer);
 		
+		temp = get_sensor("st-0");
+		ti = get_sensor("sti0");
+
+		control_na = pi_algorithm(&na_control, temp_ref, temp);
+		control_q = pi_algorithm(&q_control, temp_ref, temp);
+
+		if(ti < temp){
+			control_ni = pi_algorithm(&ni_control, temp_ref, temp);
+		} else {
+			control_ni = 0;
+		}
+
+		set_actuator(control_na, "ana");
+		set_actuator(control_q, "aq");
+		set_actuator(control_ni, "ani");
+	
 		if (received_size > 0) {
 			buffer[received_size] = '\0';
 			printf("Received message of %d bytes >>> %s\n\n", received_size, buffer);
