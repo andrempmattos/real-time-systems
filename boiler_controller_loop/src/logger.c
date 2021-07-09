6e4035d6851d4c47f2d5a4f63fb960819d07e763
Andre <andrempmattos@gmail.com>
2021-07-09 19:14:30 -0300

    boiler_loop: src: Adding a draft of the logger functions

---
+++ boiler_controller_loop/src/logger.c
@@ -0,0 +1,30 @@
+/**
+ * \brief Logger implementation.
+ * 
+ * \date 2021/08/09
+ * 
+ * \addtogroup logger
+ * \{
+ */
+
+#include "logger.h"
+
+
+
+
+
+void logger_init(void) {
+
+}
+
+void logger_add_entry(void) {
+
+}
+
+void logger_save_file(void) {
+
+}
+
+
+
+/** \} End of logger group */
---
+++ boiler_controller_loop/src/logger.h
@@ -0,0 +1,20 @@
+/**
+ * \brief Logger definitions.
+ * 
+ * \date 2021/08/09
+ * 
+ * \defgroup logger Logger
+ * \ingroup src
+ * \{
+ */
+
+#ifndef LOGGER_H_
+#define LOGGER_H_
+
+void logger_init(void);
+void logger_add_entry(void);
+void logger_save_file(void);
+
+#endif /* LOGGER_H_ */
+
+/** \} End of logger group */
