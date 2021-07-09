df3f70fa03e347ff80c9db8e5567aa1b9388cbc9
Andre <andrempmattos@gmail.com>
2021-07-09 18:47:46 -0300

    boiler_loop: src: Adding sensor and actuator functions

--- boiler_controller_loop/src/controller.c
+++ boiler_controller_loop/src/controller.c
@@ -1,8 +1,19 @@
+/**
+ * \brief Controller implementation.
+ * 
+ * \date 2021/08/09
+ * 
+ * \addtogroup controller
+ * \{
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "controller.h"
 
-struct controller controller_init(char *manipulated_variable, char *process_variable, float overflow, float underflow, float ku, float pu) {
-	struct controller cont = {
+controller_t controller_init(char *manipulated_variable, char *process_variable, float overflow, float underflow, float ku, float pu) {
+	controller_t cont = {
         .manipulated_variable = manipulated_variable,
         .process_variable = process_variable,
         .overflow = overflow,
@@ -16,8 +27,7 @@ struct controller controller_init(char *manipulated_variable, char *process_vari
     return cont;
 }
 
-
-float pi_algorithm(struct controller *cont, float reference, float control_variable) {
+float pi_algorithm(controller_t *cont, float reference, float control_variable) {
     float error;
     float control_action;
 
@@ -34,9 +44,37 @@ float pi_algorithm(struct controller *cont, float reference, float control_varia
 }
 
 float get_sensor(char *sensor) {
-	
+
+    char buffer[MAX_BUFFER_SIZE]; 
+    float value = 0;
+    int received_size = 0;
+
+    send_message(sensor);
+    received_size = receive_message(buffer);
+
+    if (received_size > 0) {
+        buffer[received_size] = '\0';
+        memcpy(buffer, buffer+3, received_size+1-3); 
+        value = atof(buffer);
+    }
+    else {
+        printf("Get sensor value failed!\n");
+    }
+
+    return value;
 }
 
-void set_actuator(float control_action, char *actuator) {
+void set_actuator(float value, char *actuator) {
 	
+    char buffer[MAX_BUFFER_SIZE];
+    char value_buffer[12];
+
+    sprintf(value_buffer, "%f", value); 
+
+    memcpy(buffer, actuator, sizeof(actuator));
+    strcat(buffer, value_buffer); 
+    
+    send_message(buffer);
 }
+
+/** \} End of controller group */
