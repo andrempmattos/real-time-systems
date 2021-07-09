ba24f4f276fe29361d5eb8410ce9d90c197698f4
Andre <andrempmattos@gmail.com>
2021-07-09 12:14:10 -0300

    boiler_loop: src: Adding boiler communication demo application

--- boiler_controller_loop/src/main.c
+++ boiler_controller_loop/src/main.c
@@ -16,12 +16,14 @@
 int main(int argc, char const *argv[]) {
 	
 	int cycle = 0;
+	char buffer[MAX_BUFFER_SIZE];
+	int received_size = 0;
 
 	/* Boiler controller initialization */
 	controller_init();
 
 	/* UDP interface initialization */
-	udp_init();
+	udp_init(SERVER_ADDRESS, SERVER_PORT);
 
 	/* Timer initialization */
 	timer_init();
@@ -29,9 +31,21 @@ int main(int argc, char const *argv[]) {
 	/* Main loop (cyclic executive) */
 	while(1) {
 		
-		/* Do stuff */
-		printf("running! \t %d \n", cycle++);
+		printf("Loop cycle! \t %d \n", cycle++);
+
+		/********* Do stuff */
+		send_message("st-0");
+		received_size = receive_message(buffer);
 		
+		if (received_size > 0) {
+			buffer[received_size] = '\0';
+			printf("Received message of %d bytes >>> %s\n\n", received_size, buffer);
+		}
+		else {
+			printf("Receive message failed!\n\n");
+		}
+		/********* Finished stuff */
+
 		/* Delay until next system loop cycle (system tick) */
 		timer_delay(SYS_TICK);
 	}
